f69e6f3a75d80cf717c2368c63e50a3b3ef72be3
Diallo <abdoulayekorsediallo@gmail.com>
2024-03-14 17:55:57 +0000

    création de mon premier api rest avec node js: avec les fonctionnalités CRUD

---
+++ app.js
@@ -0,0 +1,57 @@
+const express= require('express')
+const morgan= require('morgan')
+const favicon= require('serve-favicon')
+const bodyParser= require('body-parser')
+const helper= require('./helper')
+let pokemons= require('./mock-pokemon')
+const app= express()
+
+const port= 3000
+
+
+
+app
+.use(favicon(__dirname + '/favicon.ico'))
+.use(morgan('dev'))
+.use(bodyParser.json())
+
+app.get('/',(req,res) => res.send('hello Express'))
+
+app.get('/api/pokemons/:id',(req,res) => {
+    const id= parseInt(req.params.id)
+    const pokemon= pokemons.find(pokemon => pokemon.id===id)
+    const message= "un pokemon a été bien trouvé"
+    res.json(helper.success(message,pokemon))
+    // res.send(`Vous avez demandé le pokemon numero ${pokemon.name}`)
+})
+
+app.get('/api/pokemons',(req,res) => {
+    const message= "La liste des pokemons a été bien récuperée"
+    res.json(helper.success(message,pokemons))
+})
+
+app.post('/api/pokemons',(req,res) => {
+   const id= helper.getUniqueId(pokemons)
+   const pokemonCreated= {...req.body, ... {id:id,created:Date()}}
+   pokemons.push(pokemonCreated)
+   const message= `Le pokemon ${pokemonCreated.name} a bien été crée`
+    res.json(helper.success(message,pokemonCreated))
+})
+
+app.put('/api/pokemons/:id',(req,res) => {
+    const id= parseInt(req.params.id)
+    const pokemonUpdate= {...req.body, id:id}
+    pokemons= pokemons.map(pokemon => {
+        return pokemon.id===id ? pokemonUpdate:pokemon
+    })
+    const message= `le pokemon ${pokemonUpdate.name} a été bien modifié`
+    res.json(helper.success(message,pokemonUpdate))
+})
+
+
+// app.get('/api/pokemons',(req,res) => {
+//     res.send(`Il y a ${pokemons.length} pokemons dans le pokedex pour le moment`)
+// })
+
+app.listen(port,()=>console.log(`Notre application est demarrée sur:http:://localhost:${port}`))
+
---
+++ mock-pokemon.js
@@ -0,0 +1,94 @@
+const pokemons= [
+    {
+        "id": 1,
+        "name": "Bulbasaur",
+        "hp": 45,
+        "cp": 49,
+        "picture": "bulbasaur.jpg",
+        "type": ["Grass", "Poison"],
+        "created": Date()
+      },
+      {
+        "id": 2,
+        "name": "Charmander",
+        "hp": 39,
+        "cp": 52,
+        "picture": "charmander.jpg",
+        "type": ["Fire"],
+        "created": Date()
+      },
+      {
+        "id": 3,
+        "name": "Squirtle",
+        "hp": 44,
+        "cp": 48,
+        "picture": "squirtle.jpg",
+        "type": ["Water"],
+        "created": Date()
+      },
+      {
+        "id": 4,
+        "name": "Pikachu",
+        "hp": 35,
+        "cp": 55,
+        "picture": "pikachu.jpg",
+        "type": ["Electric"],
+        "created": Date()
+      },
+      {
+        "id": 5,
+        "name": "Jigglypuff",
+        "hp": 115,
+        "cp": 30,
+        "picture": "jigglypuff.jpg",
+        "type": ["Normal", "Fairy"],
+        "created": Date()
+      },
+      {
+        "id": 6,
+        "name": "Meowth",
+        "hp": 40,
+        "cp": 45,
+        "picture": "meowth.jpg",
+        "type": ["Normal"],
+        "created": Date()
+      },
+      {
+        "id": 7,
+        "name": "Psyduck",
+        "hp": 50,
+        "cp": 52,
+        "picture": "psyduck.jpg",
+        "type": ["Water"],
+        "created":  Date()
+      },
+      {
+        "id": 8,
+        "name": "Geodude",
+        "hp": 40,
+        "cp": 80,
+        "picture": "geodude.jpg",
+        "type": ["Rock", "Ground"],
+        "created": Date()
+      },
+      {
+        "id": 9,
+        "name": "Eevee",
+        "hp": 55,
+        "cp": 45,
+        "picture": "eevee.jpg",
+        "type": ["Normal"],
+        "created": Date()
+      },
+      {
+        "id": 10,
+        "name": "Mewtwo",
+        "hp": 106,
+        "cp": 90,
+        "picture": "mewtwo.jpg",
+        "type": ["Psychic"],
+        "created": Date()
+      }
+]
+
+module.exports = pokemons;
