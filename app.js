f76a6f3af6b32b1b641be7ab464cb560a932ac14
Diallo <abdoulayekorsediallo@gmail.com>
2024-03-16 15:10:43 +0000

    installation de l'ORM sequelize avec le driver mariadb et l'instanciation de sequelize

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express= require('express')
 const morgan= require('morgan')
 const favicon= require('serve-favicon')
 const bodyParser= require('body-parser')
+const { Sequelize } = require('sequelize');
 const helper= require('./helper')
 let pokemons= require('./mock-pokemon')
 const app= express()
 
 const port= 3000
 
+const sequelize = new Sequelize(
+    'pokedex',
+    'root',
+    '',
+    {
+        host:'localhost',
+        dialect:'mariadb',
+        dialectOptions:{
+            timezone: 'Etc/GMT-2'
+        },
+        logging: false
+    }
+)
+
+sequelize.authenticate()
+.then(_ => console.log('la connexion à la base de donnée a été bien établie'))
+.catch(error => console.log(`Impossible de se connecter à la base de donnée ${error}`))
 
 
 app
